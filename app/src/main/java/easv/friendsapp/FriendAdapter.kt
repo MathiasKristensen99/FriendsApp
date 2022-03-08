7c0509f9c78c41a0c52f4db9600c733b29c390ed
Mathias Kristensen <mathiaskristensen52@gmail.com>
2022-03-08 14:11:41 +0100

    Listview works

--- app/src/main/java/easv/friendsapp/FriendAdapter.kt
+++ app/src/main/java/easv/friendsapp/FriendAdapter.kt
@@ -1,9 +1,33 @@
 package easv.friendsapp
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import easv.friendsapp.model.BEFriend
 
 class FriendAdapter(context: Context, private val friends: Array<BEFriend>) : ArrayAdapter<BEFriend>(context, 0, friends) {
+    override fun getView(position: Int, v: View?, parent: ViewGroup): View {
+        var v1: View? = v
+        if (v1 == null) {
+            val mInflater = LayoutInflater.from(context)
+            v1 = mInflater.inflate(R.layout.cell, null)
 
+        }
+        val resView: View = v1!!
+        //resView.setBackgroundColor(colours[position % colours.size])
+        val f = friends[position]
+        val nameView = resView.findViewById<TextView>(R.id.txtName)
+        val phoneView = resView.findViewById<TextView>(R.id.txtPhone)
+        val favoriteView = resView.findViewById<ImageView>(R.id.imgFavoriteExt)
+        nameView.text = f.name
+        phoneView.text = f.phone
+
+        favoriteView.setImageResource(if (f.isFavorite) R.drawable.happy else R.drawable.angry)
+
+        return resView
+    }
 }
--- app/src/main/java/easv/friendsapp/MainActivity.kt
+++ app/src/main/java/easv/friendsapp/MainActivity.kt
@@ -2,10 +2,18 @@ package easv.friendsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import android.widget.TextView
+import easv.friendsapp.repository.FriendsRepository
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = FriendAdapter(this, FriendsRepository().getAll())
+
+        val lvFriends = this.findViewById<ListView>(R.id.lvFriends)
+        lvFriends.adapter = adapter
     }
 }
