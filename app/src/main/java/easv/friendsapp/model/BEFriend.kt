b0c0b5413d470f58c059f9346e87c016052fb2bc
Mathias Kristensen <mathiaskristensen52@gmail.com>
2022-03-22 12:08:56 +0100

    Added image entity

--- app/src/main/java/easv/friendsapp/model/BEFriend.kt
+++ app/src/main/java/easv/friendsapp/model/BEFriend.kt
@@ -2,4 +2,4 @@ package easv.friendsapp.model
 
 import java.io.Serializable
 
-data class BEFriend(var name: String, var phone: String, var isFavorite: Boolean, var address: String): Serializable
+data class BEFriend(var name: String, var phone: String, var isFavorite: Boolean, var address: String, var image: String): Serializable
--- app/src/main/java/easv/friendsapp/repository/FriendsRepository.kt
+++ app/src/main/java/easv/friendsapp/repository/FriendsRepository.kt
@@ -4,9 +4,9 @@ import easv.friendsapp.model.BEFriend
 
 class FriendsRepository {
     val friends = arrayOf<BEFriend>(
-        BEFriend("Julian", "123", false, "wdad"),
-        BEFriend("Christian", "1234", true, "wjd"),
-        BEFriend("Mathias", "12345", true, "wdjawop"),
+        BEFriend("Julian", "123", false, "wdad", ""),
+        BEFriend("Christian", "1234", true, "wjd", ""),
+        BEFriend("Mathias", "12345", true, "wdjawop", ""),
     )
 
     fun getAll():Array<BEFriend> = friends
